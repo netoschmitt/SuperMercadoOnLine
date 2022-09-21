d728299b3ab69c0b80c48d70df76456bdf51e8b5
neto schmitt <agneloschmittneto@gmail.com>
2022-09-21 16:47:42 -0300

    adicionando a classe cliente no contexto para a migration

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SuperMercadoNetoOnLine.Models;
 
 namespace SuperMercadoNetoOnLine.Data
@@ -14,6 +10,8 @@ namespace SuperMercadoNetoOnLine.Data
         {
         }
 
-        public DbSet<SuperMercadoNetoOnLine.Models.Produto> Produto { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+
+        public DbSet<Cliente> Clientes { get; set; }
     }
 }
