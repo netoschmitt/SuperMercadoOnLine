adca9684271abd6d12eec6375d0166d9b2a2d260
neto schmitt <agneloschmittneto@gmail.com>
2022-09-23 09:55:27 -0300

    entidade itemPedido

---
+++ Models/ItemPedido.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SuperMercadoNetoOnLine.Models
+{
+    public class ItemPedido
+    {
+        [Required]
+        public int IdPedido { get; set; }
+        [Required]
+        public int IdProduto  { get; set; }
+
+        [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
+        public float Quantidade { get; set; }            
+
+        [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
+        [Column(TypeName ="decimal(18,2")]
+        [Display(Name = "Valor Unitário")]
+        public decimal ValorUnitario { get; set; }
+
+        // vincula a prop pedido a este item de pedido
+        [ForeignKey("IdPedido")]
+        public Pedido Pedido { get; set; }
+        // vincula a prop de produto a item de pedido
+        [ForeignKey("IdProduto")]
+        public Produto Produto { get; set; }
+
+    }
+}
