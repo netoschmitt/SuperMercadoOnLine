be383efbf261c474aafd61b7cc26360e46977a29
neto schmitt <agneloschmittneto@gmail.com>
2022-09-23 10:45:34 -0300

    add visitado

---
+++ Models/Visitado.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace SuperMercadoNetoOnLine.Models
+{
+    public class Visitado
+    {
+        [Required]
+        public int IdCliente { get; set; }
+        [Required]
+        public int IDProduto { get; set; }
+        [Required]
+        public DateTime DataHora { get; set; }
+
+        [ForeignKey("IdCliente")]
+        public Cliente Cliente { get; set; }
+
+        [ForeignKey("IdProduto")]
+        public Produto Produto { get; set; }
+    }
+}
